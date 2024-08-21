157b0579e8152b2d27f229df90ae041d808b8b6a
Amirreza Tarabkhah <amirreza.tarabkhah@gmail.com>
2024-08-21 13:36:29 +0330

    Add first file

---
+++ resnet18.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+
+class residualBox(nn.Module):
+    def __init__(self, in_channels, channels, stride = 1, last = False):
+        super().__init__(residualBox)
+        self.last = last
+        
