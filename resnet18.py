6943f3603d73cfec89d710a48dd825a59b4011f5
Amirreza Tarabkhah <amirreza.tarabkhah@gmail.com>
2024-08-22 19:04:15 +0330

    Complete code

--- resnet18.py
+++ resnet18.py
@@ -1,35 +1,47 @@
+import numpy as np
 import torch
+import torch.backends
+import torch.backends.mps
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data.sampler import SubsetRandomSampler
 
-class residualBox(nn.Module):
+if torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+else:
+    device = "cpu"
 
-    conv1 = None
-    conv2 = None
-    def __init__(self, in_channels, out_channels, stride = 1, last = False):
-        super().__init__(residualBox)
-        self.last = last
-        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=stride, padding=1),
-                                   nn.BatchNorm2d(out_channels),
-                                   nn.ReLU())
-        self.conv2 = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3, stride=1, padding=1),
-                                   nn.BatchNorm2d(out_channels))
-        if stride != 1 or out_channels != in_channels:
-            self.shortcut = nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride),
-                                          nn.BatchNorm2d(out_channels))
-        else:
-            self.shortcut = nn.Sequential()
+print(device)
+device = torch.device(device)
+print(device)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, shortcut = None):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Sequential(
+                        nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1),
+                        nn.BatchNorm2d(out_channels),
+                        nn.ReLU())
+        self.conv2 = nn.Sequential(
+                        nn.Conv2d(out_channels, out_channels, kernel_size = 3, stride = 1, padding = 1),
+                        nn.BatchNorm2d(out_channels))
+        self.shortcut = shortcut
+        self.relu = nn.ReLU()
+        self.out_channels = out_channels
         
-        def forward(self, x):
-            out = self.conv1(x)
-            out = self.conv2(x)
-            out += self.shortcut(x)
-            preact = out
-            out = F.relu(out)
-            if self.is_last:
-                return out, preact
-            else:
-                return out
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.conv2(out)
+        if self.shortcut:
+            residual = self.shortcut(x)
+        out += residual
+        out = self.relu(out)
+        return out
             
 class ResNet(nn.Module):
     def __init__(self, block, layers, num_classes = 10):
@@ -48,15 +60,15 @@ class ResNet(nn.Module):
         self.fc = nn.Linear(512, num_classes)
         
     def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
+        shortcut = None
         if stride != 1 or self.inplanes != planes:
             
-            downsample = nn.Sequential(
+            shortcut = nn.Sequential(
                 nn.Conv2d(self.inplanes, planes, kernel_size=1, stride=stride),
                 nn.BatchNorm2d(planes),
             )
         layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
+        layers.append(block(self.inplanes, planes, stride, shortcut))
         self.inplanes = planes
         for i in range(1, blocks):
             layers.append(block(self.inplanes, planes))
@@ -77,3 +89,144 @@ class ResNet(nn.Module):
         x = self.fc(x)
 
         return x
+    
+def data_loader(data_dir,
+                batch_size,
+                random_seed=42,
+                valid_size=0.1,
+                shuffle=True,
+                test=False):
+  
+    normalize = transforms.Normalize(
+        mean=[0.4914, 0.4822, 0.4465],
+        std=[0.2023, 0.1994, 0.2010],
+    )
+
+    # define transforms
+    transform = transforms.Compose([
+            transforms.Resize((224,224)),
+            transforms.ToTensor(),
+            normalize,
+    ])
+
+    if test:
+        dataset = datasets.CIFAR10(
+          root=data_dir, train=False,
+          download=True, transform=transform,
+        )
+
+        data_loader = torch.utils.data.DataLoader(
+            dataset, batch_size=batch_size, shuffle=shuffle
+        )
+
+        return data_loader
+
+    # load the dataset
+    train_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    valid_dataset = datasets.CIFAR10(
+        root=data_dir, train=True,
+        download=True, transform=transform,
+    )
+
+    num_train = len(train_dataset)
+    indices = list(range(num_train))
+    split = int(np.floor(valid_size * num_train))
+
+    if shuffle:
+        np.random.seed(42)
+        np.random.shuffle(indices)
+
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, sampler=train_sampler)
+ 
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=batch_size, sampler=valid_sampler)
+
+    return (train_loader, valid_loader)
+
+
+# CIFAR10 dataset 
+train_loader, valid_loader = data_loader(data_dir='./data',
+                                         batch_size=64)
+
+test_loader = data_loader(data_dir='./data',
+                              batch_size=64,
+                              test=True)
+
+def set_model():
+    num_classes = 10
+    num_epochs = 20
+    batch_size = 16
+    learning_rate = 0.01
+
+    model = ResNet(ResidualBlock, [2, 2, 2, 2], num_classes).to(device)
+
+    # Loss and optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, weight_decay = 0.001, momentum = 0.9)  
+
+    # Train the model
+    total_step = len(train_loader)
+
+    import gc
+    total_step = len(train_loader)
+
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(train_loader):  
+            # Move tensors to the configured device
+            images = images.to(device)
+            labels = labels.to(device)
+            
+            # Forward pass
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            
+            # Backward and optimize
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            del images, labels, outputs
+            torch.cuda.empty_cache()
+            gc.collect()
+
+        print ('Epoch [{}/{}], Loss: {:.4f}' 
+                    .format(epoch+1, num_epochs, loss.item()))
+                
+        # Validation
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, labels in valid_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                del images, labels, outputs
+        
+            print('Accuracy of the network on the {} validation images: {} %'.format(5000, 100 * correct / total)) 
+        
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, labels in test_loader:
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = model(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                del images, labels, outputs
+
+            print('Accuracy of the network on the {} test images: {} %'.format(10000, 100 * correct / total))
+
+set_model()
